ededa8d71c440d5e270e97a0f4b6a880b4f6af9d
mexisme <wildjim+dev@kiwinet.org>
2017-10-17 22:35:34 +1300

    Add default (empty) config tests

---
+++ logging/logging_test.go
@@ -0,0 +1,12 @@
+package logging_test
+
+import (
+	. "github.com/mexisme/go-config/logging"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Logging", func() {
+
+})
---
+++ settings/settings_test.go
@@ -0,0 +1,12 @@
+package settings_test
+
+import (
+	. "github.com/mexisme/go-config/settings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("config/settings", func() {
+
+})
