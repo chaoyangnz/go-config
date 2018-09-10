cacf2db7b99f95099494e37685ea2faf34d75b16
mexisme <wildjim+dev@kiwinet.org>
2018-09-10 21:55:48 +1200

    Add forwarders for viper

--- getters.go
+++ getters.go
@@ -1,18 +1,77 @@
 package config
 
 import (
-	log "github.com/sirupsen/logrus"
+	"time"
 
-	"github.com/mexisme/go-config/logging"
 	"github.com/mexisme/go-config/settings"
 )
 
 // AddConfigItems passes the configItems through to settings.AddConfigItems()
-func AddConfigItems(configItems []string) {
-	settings.AddConfigItems(configItems)
+func AddConfigItems(configKeys []string) {
+	settings.AddConfigItems(configKeys)
 }
 
 // ApplyWith passes the configItems through to settings.ApplyWith()
-func ApplyWith(item string, f func(interface{})) {
-	settings.ApplyWith(item, f)
+func ApplyWith(key string, f func(interface{})) {
+	settings.ApplyWith(key, f)
+}
+
+// Get forwards to settings.Get
+func Get(key string) interface{} {
+	return settings.Get(key)
+}
+
+// GetBool forwards to settings.GetBool
+func GetBool(key string) bool {
+	return settings.GetBool(key)
+}
+
+// GetFloat64 forwards to settings.GetFloat64
+func GetFloat64(key string) float64 {
+	return settings.GetFloat64(key)
+}
+
+// GetInt forwards to settings.GetInt
+func GetInt(key string) int {
+	return settings.GetInt(key)
+}
+
+// GetString forwards to settings.GetString
+func GetString(key string) string {
+	return settings.GetString(key)
+}
+
+// GetStringMap forwards to settings.GetStringMap
+func GetStringMap(key string) map[string]interface{} {
+	return settings.GetStringMap(key)
+}
+
+// GetStringMapString forwards to settings.GetStringMapString
+func GetStringMapString(key string) map[string]string {
+	return settings.GetStringMapString(key)
+}
+
+// GetStringSlice forwards to settings.GetStringSlice
+func GetStringSlice(key string) []string {
+	return settings.GetStringSlice(key)
+}
+
+// GetTime forwards to settings.GetTime
+func GetTime(key string) time.Time {
+	return settings.GetTime(key)
+}
+
+// GetDuration forwards to settings.GetDuration
+func GetDuration(key string) time.Duration {
+	return settings.GetDuration(key)
+}
+
+// IsSet forwards to settings.IsSet
+func IsSet(key string) bool {
+	return settings.IsSet(key)
+}
+
+// AllSettings forwards to settings.AllSettings
+func AllSettings() map[string]interface{} {
+	return settings.AllSettings()
 }
--- settings/getters.go
+++ settings/getters.go
@@ -1,17 +1,81 @@
 package settings
 
-import "github.com/spf13/viper"
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 // AddConfigItems adds a new configuration item, and makes it overridable by env vars
-func AddConfigItems(configItems []string) {
-	for _, item := range configItems {
-		viper.BindEnv(item)
+func AddConfigItems(configKeys []string) {
+	for _, key := range configKeys {
+		viper.BindEnv(key)
 	}
 }
 
 // ApplyWith gets a setting from viper, and passes it to a closure
-func ApplyWith(item string, f func(interface{})) {
-	if viper.IsSet(item) {
-		f(viper.Get(item))
+func ApplyWith(key string, f func(interface{})) {
+	if viper.IsSet(key) {
+		f(viper.Get(key))
 	}
+}
+
+// Get forwards to viper.Get
+func Get(key string) interface{} {
+	return viper.Get(key)
+}
+
+// GetBool forwards to viper.GetBool
+func GetBool(key string) bool {
+	return viper.GetBool(key)
+}
+
+// GetFloat64 forwards to viper.GetFloat64
+func GetFloat64(key string) float64 {
+	return viper.GetFloat64(key)
+}
+
+// GetInt forwards to viper.GetInt
+func GetInt(key string) int {
+	return viper.GetInt(key)
+}
+
+// GetString forwards to viper.GetString
+func GetString(key string) string {
+	return viper.GetString(key)
+}
+
+// GetStringMap forwards to viper.GetStringMap
+func GetStringMap(key string) map[string]interface{} {
+	return viper.GetStringMap(key)
+}
+
+// GetStringMapString forwards to viper.GetStringMapString
+func GetStringMapString(key string) map[string]string {
+	return viper.GetStringMapString(key)
+}
+
+// GetStringSlice forwards to viper.GetStringSlice
+func GetStringSlice(key string) []string {
+	return viper.GetStringSlice(key)
+}
+
+// GetTime forwards to viper.GetTime
+func GetTime(key string) time.Time {
+	return viper.GetTime(key)
+}
+
+// GetDuration forwards to viper.GetDuration
+func GetDuration(key string) time.Duration {
+	return viper.GetDuration(key)
+}
+
+// IsSet forwards to viper.IsSet
+func IsSet(key string) bool {
+	return viper.IsSet(key)
+}
+
+// AllSettings forwards to viper.AllSettings
+func AllSettings() map[string]interface{} {
+	return viper.AllSettings()
 }
